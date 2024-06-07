c7fd4f23915f7917812d117fe3040b42906f4721
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-06-07 15:46:10 +0200

    Added new routes and links for Host dashboard, income, and reviews

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Vans from './pages/Vans/Vans';
 import './server'
 import VanDetail from './pages/Vans/VanDetail';
 import Layout from './components/Layout'
+import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
 
 export default function App() {
   return (
@@ -19,6 +22,9 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
         
       </Routes>
