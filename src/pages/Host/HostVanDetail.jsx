89583840f2d58d47e6fab43078589ca4d0432ce7
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-06-08 21:55:24 +0200

    Refactor HostVans and HostVanDetail components: moved van list logic to HostVans, added useParams to HostVanDetail, and updated JSX for both components

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,46 +1,27 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
-export default function HostVans() {
-    const [vans, setVans] = React.useState([])
+export default function HostVanDetail() {
+
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
 
     React.useEffect(() => {
-        fetch("/api/host/vans")
+        fetch(`/api/host/vans/${id}`)
             .then(res => res.json())
-            .then(data => setVans(data.vans))
+            .then(data => setCurrentVan(data.vans))
     }, [])
-
-    const hostVansEls = vans.map(van => (
-        <Link
-            to={`/host/vans/${van.id}`}
-            key={van.id}
-            className="host-van-link-wrapper"
-        >
-            <div className="host-van-single" key={van.id}>
-                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-                <div className="host-van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}/day</p>
-                </div>
-            </div>
-        </Link>
-    ))
+    
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
 
     return (
-        <section>
-            <h1 className="host-vans-title">Your listed vans</h1>
-            <div className="host-vans-list">
-                {
-                    vans.length > 0 ? (
-                        <section>
-                            {hostVansEls}
-                        </section>
-
-                    ) : (
-                            <h2>Loading...</h2>
-                        )
-                }
-            </div>
-        </section>
+        <div>
+            <img src={currentVan.imageUrl} width={150} />
+            <h2>{currentVan.name}</h2>
+            <p>{currentVan.price}</p>
+            <p>{currentVan.type}</p>
+        </div>
     )
 }
