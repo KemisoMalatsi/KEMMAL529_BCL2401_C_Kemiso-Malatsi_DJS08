be3b2838aa5f3a21e24f795cce3b4e9ce1a203d8
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-06-08 20:12:01 +0200

    Updated route paths and added navigation links to HostLayout component

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -4,7 +4,11 @@ import { Link, Outlet } from "react-router-dom"
 export default function HostLayout() {
     return (
         <>
-            <header />
+            <nav className="host-nav">
+                <Link to="/host">Dashboard</Link>
+                <Link to="/host/income">Income</Link>
+                <Link to="/host/reviews">Reviews</Link>
+            </nav>
             <Outlet />
         </>
     )
