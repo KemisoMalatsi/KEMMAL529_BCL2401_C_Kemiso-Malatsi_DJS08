89d192c6115781a27bcbb38c419c94abb4dc9745
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-06-08 20:04:19 +0200

    "Removed unnecessary whitespace and reformatted imports in App.jsx; removed navigation links from HostLayout.jsx and replaced with empty header element."

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -4,11 +4,7 @@ import { Link, Outlet } from "react-router-dom"
 export default function HostLayout() {
     return (
         <>
-            <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
-            </nav>
+            <header />
             <Outlet />
         </>
     )
