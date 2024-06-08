e27dbbefb3cfae6395361044654504936b34c435
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-06-08 19:39:14 +0200

    Wrap host routes with HostLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Layout from './components/Layout'
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
+import HostLayout from './components/HostLayout';
 
 export default function App() {
   return (
@@ -23,7 +24,8 @@ export default function App() {
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
 
-          <Route path="/host" element={<Dashboard />}>
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
